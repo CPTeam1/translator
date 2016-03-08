121d3fa71f9d5a38ac2259e3c39b3bbe7326d38d
Ishan Pande <ishanpande@gmail.com>
2016-03-08 08:14:11 -0800

    Added imageview for asking a question, TODO - scaling bitmap efficiently, adding unique qsID to photo URI

--- Android/app/src/main/java/com/cp1/translator/activities/AskQuestion.java
+++ Android/app/src/main/java/com/cp1/translator/activities/AskQuestion.java
@@ -1,37 +1,56 @@
 package com.cp1.translator.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cp1.translator.R;
 import com.cp1.translator.models.Entry;
 import com.cp1.translator.models.Post;
 import com.cp1.translator.models.Question;
 import com.cp1.translator.models.User;
+import com.cp1.translator.utils.Constants;
 import com.parse.ParseException;
 import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 import org.w3c.dom.Text;
 
+import java.io.File;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
+import static com.cp1.translator.utils.Constants.*;
 
 public class AskQuestion extends AppCompatActivity {
     @Bind(R.id.btAskQs) Button btAskQs;
     @Bind(R.id.etQs) EditText etQs;
     @Bind(R.id.tvChars) TextView tvCharsLeft;
+    @Nullable @Bind(R.id.ivQsPic) ImageView ivQsPic;
+
     int textColor;
 
     private static int QS_CHAR_LIMIT = 140;
     private static boolean qsPosted = false;
+    private String imageURI;
+    private String audioURI;
+    private String videoURI;
 
 
     @Override
@@ -56,8 +75,8 @@ public class AskQuestion extends AppCompatActivity {
 
                     Question qsDB  = saveLocally(question,User.getCurrentUser().getEmail());
 
-                    saveToParse(question);
-
+                    // TODO uncomment once model is fixed
+                    //saveToParse(question);
 
                     Intent displayQsIntent = new Intent(getApplicationContext(), MainActivity.class);
                     displayQsIntent.putExtra("question", Parcels.wrap(qsDB));
@@ -67,8 +86,76 @@ public class AskQuestion extends AppCompatActivity {
         });
     }
 
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+
+                imageURI = takenPhotoUri.toString();
+                Log.d(APP_TAG,"qs image uri = " + imageURI);
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+
+                // Load the taken image into a preview
+                ivQsPic.setImageBitmap(takenImage);
+
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public Uri getPhotoFileUri(String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(
+                    getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    // Returns true if external storage for photos is available
+    private boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
+    }
+
+
+
     private Question saveLocally(String question, String userName) {
-        return Question.toQuestion(question,userName);
+        Question q = Question.toQuestion(question,userName);
+        if(q!=null && imageURI!=null)
+            q.setImageURI(imageURI);
+        return q;
     }
 
     private void saveToParse(String question) {
--- Android/app/src/main/java/com/cp1/translator/utils/Constants.java
+++ Android/app/src/main/java/com/cp1/translator/utils/Constants.java
@@ -5,4 +5,6 @@ package com.cp1.translator.utils;
  */
 public class Constants {
     public static final String APP_TAG = "socialtranslator";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public static String photoFileName = "photo.jpg";
 }
