5a2013e09c25b552e2bb73ada8e7aae86f852d7f
kimhy08 <Hyunji.Kim@ca.com>
2016-03-09 11:03:15 -0800

    skeleton code: ListView for questions

--- Android/app/src/main/java/com/cp1/translator/fragments/PageFragment.java
+++ Android/app/src/main/java/com/cp1/translator/fragments/PageFragment.java
@@ -1,26 +1,26 @@
 package com.cp1.translator.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.cp1.translator.R;
-import com.cp1.translator.activities.AskQuestion;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 // In this case, the fragment displays simple text based on the page
 public class PageFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
 
-    private String mTitle;
+//    private String mTitle;
 
-    private Button btAskQs;
+//    private Button btAskQs;
+    @Bind(R.id.tvQuestionTitle) TextView tvQuestionTitle;
+    @Bind(R.id.tvRepliesCount) TextView tvRepliesCount;
 
     public static PageFragment newInstance(String title) {
         Bundle args = new Bundle();
@@ -33,7 +33,7 @@ public class PageFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mTitle = getArguments().getString(ARG_PAGE);
+//        mTitle = getArguments().getString(ARG_PAGE);
     }
 
     // Inflate the fragment layout we defined above for this fragment
@@ -45,20 +45,13 @@ public class PageFragment extends Fragment {
         if (true) { // here if condition MUST CHANGE!! It must check if the question list is empty
             view = inflater.inflate(R.layout.fragment_empty, container, false);
         } else {
-            view = inflater.inflate(R.layout.fragment_page, container, false);
-            ButterKnife.bind(view);
-            TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
-            btAskQs = (Button) view.findViewById(R.id.btQs);
+            view = inflater.inflate(R.layout.fragment_questions, container, false);
+            ButterKnife.bind(this, view);
 
-            tvTitle.setText("Fragment " + mTitle);
+            // TODO
+            /********************** ListView with Adapter **********************/
 
-            btAskQs.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent askQsIntent = new Intent(getActivity(), AskQuestion.class);
-                    startActivity(askQsIntent);
-                }
-            });
+            /********************** end of ListView **********************/
         }
 
         return view;
