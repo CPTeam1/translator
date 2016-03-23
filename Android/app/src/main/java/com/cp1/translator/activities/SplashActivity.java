1ca51a004090f5e5cbe8d440b5097e454a751cdf
Hyunji Kim <hj.eunice@gmail.com>
2016-03-22 21:34:48 -0700

    Done: Issue #41

---
+++ Android/app/src/main/java/com/cp1/translator/activities/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.cp1.translator.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.cp1.translator.R;
+import com.cp1.translator.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final ImageView ivAppIcon = (ImageView) findViewById(R.id.ivAppIcon);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(SplashActivity.this, (View)ivAppIcon, "appIcon");
+                startActivity(intent);
+
+                // close this Activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
